17606588f09e0e833b711740e6baa8daa8b8e7e9
David Mooter <theexternvoid@gmail.com>
2024-02-17 20:17:41 -0500

    Change sig 1 format.

--- outlook-set-signature/src/utilities.js
+++ outlook-set-signature/src/utilities.js
@@ -20,17 +20,19 @@ function get_template_A_base_str(user_info, quote)
 {
   let str = "";
   str += '<div style="font:11px Arial, Verdana, sans-serif;color:#333">';
-  str += '<p>:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</p>';
-  str += '<p><strong><span style="font-size:12.25px">FORRESTER</span></strong><br/>';
-  str += '<span style="font:10px Arial, Verdana, sans-serif;color: #3BB982;">BOLD AT WORK</span></p>';
-  str += '<p><strong>' + user_info.name + '</strong><br/>';
+  str += ':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::<br>';
+  str += '<br>';
+  str += '<strong><span style="font-size:12.25px">FORRESTER</span></strong><br/>';
+  str += '<span style="font:10px Arial, Verdana, sans-serif;color: #3BB982;">BOLD AT WORK</span>';
+  str += '<br>';
+  str += '<strong>' + user_info.name + '</strong><br/>';
   str += user_info.job_title + (is_valid_data(user_info.phone) ? ' | Office landline: ' + user_info.phone : '') + ' | <a href="' + user_info.email + '">' + user_info.email + '</a><br/>';
   str += (is_valid_data(user_info.blog_link) ? '<a href="' + user_info.blog_link + '">My blog</a> | ' : '') + (is_valid_data(user_info.linkedin_link) ? '<a href="' + user_info.linkedin_link + '">LinkedIn profile</a>' : '') + (is_valid_data(user_info.follow_research_link) ? ' | <a href="' + user_info.follow_research_link + '">Follow my latest research</a><br/>' : '');
   str += '<br/>';
   str += '<strong>Forrester Research, Inc.</strong><br/>';
   str += '60 Acorn Park Drive, Cambridge, MA 02140 United States<br/>';
-  str += '<a href="http://www.forrester.com/">Forrester.com</a> | <a href="http://blogs.forrester.com/">Blogs</a> | <a href="http://forr.com/what-it-means">Podcasts</a> | <a href="http://twitter.com/forrester">Twitter</a> | <a href="http://linkedin.com/company/forrester-research">LinkedIn</a> | <a href="http://www.youtube.com/user/forresterresearch">YouTube</a><p>';
-  str += (is_valid_data(quote) ? '<p><br><span style="font-size:7.0pt;font-family:Arial,sans-serif">' + quote + '</span></p>' : '');
+  str += '<a href="http://www.forrester.com/">Forrester.com</a> | <a href="http://blogs.forrester.com/">Blogs</a> | <a href="http://forr.com/what-it-means">Podcasts</a> | <a href="http://twitter.com/forrester">Twitter</a> | <a href="http://linkedin.com/company/forrester-research">LinkedIn</a> | <a href="http://www.youtube.com/user/forresterresearch">YouTube</a>';
+  str += (is_valid_data(quote) ? '<br><br><span style="font-size:7.0pt;font-family:Arial,sans-serif">' + quote + '</span>' : '');
 
   return str;
 }
